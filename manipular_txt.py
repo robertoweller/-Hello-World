97fa09a327a445f073d3d540c946f96ad6af3e14
Roberto Ribeiro Weller <33500297+robertoweller@users.noreply.github.com>
2019-10-13 06:23:31 +0000

    Basicao de manipulacao de documento de texto
    Cria um documento de texto com formato de ".txt" do 1 ao 50

---
+++ manipular_txt.py
@@ -0,0 +1,55 @@
+documento = open('doc.txt','w')
+
+for q in range(1, 50):
+	documento.write('''
+------------------------------------------
+{}.
+
+Nome: 
+
+Idade: 
+
+CPF: 
+
+RG: 
+
+Data nasc: 
+
+Signo: 
+
+Mae: 
+
+Pai: 
+
+Email: 
+
+Senha: 
+
+Cep: 
+
+Endereco: 
+
+Numero: 
+
+Bairro: 
+
+Cidade: 
+
+Estado:
+
+Telefone fixo: 
+
+Celular: 
+
+Altura: 
+
+Peso: 
+
+Tipo Saguineo: 
+
+Cor: 
+
+------------------------------------------
+	'''.format(q))
+
+documento.close()
